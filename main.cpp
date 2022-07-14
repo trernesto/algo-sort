9ae9f95bfbdd5dc11289f7eddabcdd36528b6b28
trernesto <Ernest.Math.Yu@gmail.com>
2022-07-15 04:23:28 +0500

    main modification
    added bubble sort implementation into main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "bubble.h"
 
 int main() {
-	std::cout << "First commit!";
+	std::vector<double> vec = {1, 2, 5, 14, 32, 2, 0, -5, 6, 17, 44, 52, 11};
+	sortVector_bubble(vec);
+	for (int i = 0; i < vec.size(); ++i) {
+		std::cout << vec[i] << " ";
+	}
 	return 0;
 }
