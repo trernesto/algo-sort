0536d801b397f2dea93050807720a3d2a1e28cf6
trernesto <Ernest.Math.Yu@gmail.com>
2022-07-16 01:53:59 +0500

    added Heap
    Heap sort realization

---
+++ Heap.h
@@ -0,0 +1,83 @@
+#pragma once
+#include <vector>
+
+/*
+binary heap
+parent: i element
+child: 2i + 1, 2i + 2;
+insert O(log n)
+get min O(1)
+remove min O(log n)
+ */
+
+template <typename T>
+class Heap {
+private:
+	std::vector<T> heap;
+public:
+	Heap(std::vector<T> vec) {
+		//O(nlogn)
+		for (int i = 0; i < vec.size(); ++i) {
+			insert(vec[i]);
+		}
+	}
+
+	void operator = (const Heap& heap) {
+		this.heap = heap;
+	}
+
+	void insert(T x) {
+		int i = heap.size();
+		heap.push_back(x);
+		//sift up
+		//swap child and parent until heap return to normal
+		while (i > 0 && heap[i] < heap[(i - 1) / 2]) {
+			T temp = heap[i];
+			heap[i] = heap[(i - 1) / 2];
+			heap[(i - 1) / 2] = temp;
+			i = (i - 1) / 2;
+		}
+	}
+
+	T get_min() {
+		return heap[0];
+	}
+
+	void remove_min() {
+		heap[0] = heap[heap.size() - 1];
+		heap.pop_back();
+		int i = 0;
+		while (2 * i + 1 < heap.size()) {
+			int j = 2*i + 1;
+			if (2*i + 2 < heap.size() && heap[2*i + 2] < heap[j]) {
+				j = 2*i + 2;
+			}
+			if (heap[i] < heap[j]) {
+				break;
+			} else {
+				T temp = heap[i];
+				heap[i] = heap[j];
+				heap[j] = temp;
+				i = j;
+			}
+		}		
+	}
+
+	std::vector<T> sortVector_heap() {
+		Heap tempHeap = heap;
+		std::vector<T> sortedVec;
+		while (tempHeap.size() > 0) {
+			sortedVec.push_back(tempHeap.get_min());
+			tempHeap.remove_min();
+		}
+		return sortedVec;
+	}
+
+	T getElement(int i) {
+		return heap[i];
+	}
+
+	long size() {
+		return heap.size();
+	}
+};
--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include "bubble.h"
 #include "merge.h"
+#include "Heap.h"
 
 int main() {
 	std::vector<double> vec = {1, 2, 5, 14, 32, 2, 0, -5, 6, 17, 44, 52, 11};
+	Heap heap = Heap(vec);
 	//sortVector_bubble(vec);
-	vec = sortVector_merge(vec);
+	//vec = sortVector_merge(vec);
+	for (int i = 0; i < heap.size(); ++i) {
+		std::cout << heap.getElement(i) << " ";
+	}
+	std::cout << std::endl;
+	
+	vec = heap.sortVector_heap();
 	for (int i = 0; i < vec.size(); ++i) {
 		std::cout << vec[i] << " ";
 	}
+	
 	return 0;
 }
